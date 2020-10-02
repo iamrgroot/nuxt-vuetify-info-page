c7188b8237a4c03809a57f1801d340e00d91b4ac
Robert Groot <8398505+IAmRGroot@users.noreply.github.com>
2020-10-02 09:54:27 +0200

    Update vuetify.options.js

--- app/plugins/vuetify.options.js
+++ app/plugins/vuetify.options.js
@@ -1,6 +1,7 @@
 import {
     mdiAccount,
     mdiEmail,
+    mdiGithub
 } from '@mdi/js';
 
 export default {
@@ -9,6 +10,7 @@ export default {
         values: {
             mdiAccount,
             mdiEmail,
+            mdiGithub
         }
     },
 }
